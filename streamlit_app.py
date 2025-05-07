a642c2259b7bc4fb5065b4f50f82d07e78e69da9
Alex-From-Galax <162296186+Alex-From-Galax@users.noreply.github.com>
2025-05-07 19:25:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ ingredients_list = st.multiselect(
 
 
 #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response)
 
 if ingredients_list:
    # st.write(ingredients_list)
